c7ab3e8bd818804d66a4deb1da1b22fe25df829f
Skye Doto <sdoto@northwell.edu>
2025-04-04 19:57:46 -0400

    Good division display; RFTS finished; flags

---
+++ dashboard/season/definitions.js
@@ -0,0 +1,43 @@
+export const seasons = [
+    {
+        name: "April 2025",
+        divisions: [
+            'blue',
+            'red',
+            'yellow'
+        ],
+        players: {
+            'blue': [
+                'Theman',
+                'Madeline',
+                'shelby',
+                'Alecs Dyno'
+            ],
+            'red': [
+                'ad',
+                'Ari',
+                'The Hidden Ninja',
+                'Random Name',
+                'Misty',
+                'julott',
+            ],
+            'yellow': [
+                'PocketWraith',
+                'Bricky',
+                'goofgoose',
+                'Tomycarmo3',
+                'maybeGVST',
+                '9rg',
+                'Catman',
+                'Tnt_go_bang',
+                'Nene',
+            ]
+        },
+        meta: {
+            'season': {},
+            'blue': {},
+            'red': {},
+            'yellow': {}
+        }
+    }
+]
